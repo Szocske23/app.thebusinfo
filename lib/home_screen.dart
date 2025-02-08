75fb418fac37dc3c8dd56551b83a65cda94f12da
Szöcske <szocs.csongor0923@gmail.com>
2025-02-08 22:03:09 +0200

    refactor layout and spacing for improved user experience across components

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:app_thebusinfo/settings_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
@@ -89,6 +90,11 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> closestStops = [];
   Timer? debounceTimer;
   PointAnnotation? locationIndicator; // Custom location marker
+
+  List<Map<String, dynamic>> searchStops = [];
+  List<Map<String, dynamic>> searchCityes = [];
+  List<Map<String, dynamic>> searchPois = [];
+  TextEditingController searchController = TextEditingController();
 
   static const String stopsApiUrl = 'https://api.thebus.info/v1/stops';
 
@@ -271,6 +277,36 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<void> fetchSearchResults(String query) async {
+    if (query.length < 3) {
+      searchStops = [];
+      searchCityes = [];
+      searchPois = [];
+      return;
+    }
+
+    try {
+      final response = await http
+          .get(Uri.https('api.thebus.info', '/v1/search', {'query': query}));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print(data);
+        setState(() {
+          searchStops = List<Map<String, dynamic>>.from(data['stops']);
+          print(searchStops);
+          searchCityes = List<Map<String, dynamic>>.from(data['cities']);
+          print(searchCityes);
+          searchPois = List<Map<String, dynamic>>.from(data['pois']);
+          print(searchPois);
+        });
+      }
+    } catch (e) {
+      print(e);
+      _showError('Search failed: $e');
+    }
+  }
+
   Future<void> _updateCamera(LocationData locationData) async {
     if (locationData.latitude != null && locationData.longitude != null) {
       mapboxMap.location.updateSettings(LocationComponentSettings(
@@ -401,26 +437,44 @@ class _HomeScreenState extends State<HomeScreen> {
                         const SizedBox(width: 10),
                         Expanded(
                           child: TextField(
+                            controller: searchController,
                             style: const TextStyle(color: Colors.white),
-                            keyboardAppearance: Brightness.dark,
                             decoration: const InputDecoration(
-                              hintText: 'Search for destination...',
-                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: 'Cauta destinatie...',
                               border: InputBorder.none,
                             ),
-                            onChanged: (value) {
-                              // Handle search query updates
-                              print('Search query: $value');
-                            },
+                            onChanged: fetchSearchResults,
+                            keyboardType: TextInputType.text,
+                            keyboardAppearance: Brightness.dark,
+                            autocorrect: false,
+                            textInputAction: TextInputAction.done,
                           ),
                         ),
                         IconButton(
                           onPressed: () {
-                            // Add logic for clearing the search or submitting it
-                            print('Search icon pressed');
+                            if (searchStops.isNotEmpty ||
+                                searchCityes.isNotEmpty ||
+                                searchPois.isNotEmpty) {
+                              // Clear the search results
+                              setState(() {
+                                searchStops.clear();
+                                searchCityes.clear();
+                                searchPois.clear();
+                                searchController.clear();
+                              });
+                            } else {
+                              // Add logic for submitting the search if needed
+                              print('Search icon pressed');
+                            }
                           },
-                          icon: const FaIcon(
-                            FontAwesomeIcons.magnifyingGlass,
+                          icon: Icon(
+                            (searchStops.isNotEmpty ||
+                                    searchCityes.isNotEmpty ||
+                                    searchPois.isNotEmpty)
+                                ? Icons
+                                    .close // X icon when search results exist
+                                : FontAwesomeIcons
+                                    .magnifyingGlass, // Search icon when no results
                             size: 20,
                             color: Colors.white,
                           ),
@@ -477,25 +531,89 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          Positioned(
-            bottom: 100,
-            left: 0,
-            right: 0,
-            child: SizedBox(
-              height: 90,
-              child: PageView.builder(
-                itemCount: closestStops.length,
-                controller: PageController(viewportFraction: 0.88),
-                itemBuilder: (context, index) {
-                  final stop = closestStops[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 7),
-                    child: _buildStopView(context, stop),
-                  );
-                },
+          if (searchStops.isNotEmpty ||
+              searchCityes.isNotEmpty ||
+              searchPois.isNotEmpty)
+            Positioned(
+              bottom: 100,
+              left: 0,
+              right: 0,
+              // Adjust the height as needed to fit all cards
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    if (searchStops.isNotEmpty)
+                      SizedBox(
+                        height: 90,
+                        child: PageView.builder(
+                          itemCount: searchStops.length,
+                          controller: PageController(viewportFraction: 0.9),
+                          itemBuilder: (context, index) {
+                            final stop = searchStops[index];
+                            return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 7),
+                                child: _buildSearchCard(stop, 'Stop', context));
+                          },
+                        ),
+                      ),
+                    if (searchCityes.isNotEmpty) const SizedBox(height: 10),
+                    if (searchCityes.isNotEmpty)
+                      SizedBox(
+                        height: 90,
+                        child: PageView.builder(
+                          itemCount: searchCityes.length,
+                          controller: PageController(viewportFraction: 0.9),
+                          itemBuilder: (context, index) {
+                            final city = searchCityes[index];
+                            return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 7),
+                                child: _buildSearchCard(city, 'City', context));
+                          },
+                        ),
+                      ),
+                    if (searchPois.isNotEmpty) const SizedBox(height: 10),
+                    if (searchPois.isNotEmpty)
+                      SizedBox(
+                        height: 90,
+                        child: PageView.builder(
+                          itemCount: searchPois.length,
+                          controller: PageController(viewportFraction: 0.9),
+                          itemBuilder: (context, index) {
+                            final poi = searchPois[index];
+
+                            return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 7),
+                                child: _buildSearchCard(poi, 'POI', context));
+                          },
+                        ),
+                      ),
+                  ],
+                ),
               ),
             ),
-          ),
+          if (searchStops.isEmpty && searchCityes.isEmpty && searchPois.isEmpty)
+            Positioned(
+              bottom: 100,
+              left: 0,
+              right: 0,
+              child: SizedBox(
+                height: 90,
+                child: PageView.builder(
+                  itemCount: closestStops.length,
+                  controller: PageController(viewportFraction: 0.88),
+                  itemBuilder: (context, index) {
+                    final stop = closestStops[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 7),
+                      child: _buildStopView(context, stop),
+                    );
+                  },
+                ),
+              ),
+            ),
         ],
       ),
     );
@@ -652,4 +770,218 @@ Color hexStringToColor(String hexColor) {
   }
   buffer.write(hexColor);
   return Color(int.parse(buffer.toString(), radix: 16));
+}
+
+Widget _buildSearchCard(dynamic item, String type, BuildContext context) {
+  return GestureDetector(
+    onTap: () {
+      if (type == 'Stop') {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => StopDetailsPage(stopId: item['id']),
+          ),
+        );
+      }
+      ;
+    },
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x20e2861d),
+            spreadRadius: 2,
+            blurRadius: 20,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.only(left: 15, top: 15, right: 20, bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: [
+                if (type == 'Stop')
+                  const Icon(
+                    FontAwesomeIcons.busSimple,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                if (type == 'Stop')
+                  const SizedBox(
+                    width: 6,
+                  ),
+                Text(
+                  '${item["name"]}',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  overflow: TextOverflow.fade,
+                ),
+                if (type == 'Stop')
+                  Text(
+                    ' - ${item["city"]}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white70,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    overflow: TextOverflow.fade,
+                  ),
+              ]),
+              if (type == 'POI')
+                const Icon(
+                  FontAwesomeIcons.mapLocationDot,
+                  color: Colors.white,
+                  size: 25,
+                ),
+              if (type == 'City')
+                const Icon(
+                  FontAwesomeIcons.city,
+                  color: Colors.white,
+                  size: 25,
+                )
+            ],
+          ),
+          const SizedBox(height: 8),
+          if (type == 'POI')
+            Text(
+              item["address"],
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          if (type == 'City')
+            Text(
+              item["zipcode"],
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          if (type == 'Stop')
+            if (item["routes"] != null && item["routes"].isNotEmpty)
+              Row(
+                children: [
+                  Container(
+                    height: 28,
+                    padding: const EdgeInsets.all(5),
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(6),
+                        bottomRight: Radius.circular(4),
+                        bottomLeft: Radius.circular(10),
+                        topRight: Radius.circular(4),
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.directions_bus,
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: Wrap(
+                      spacing: 4,
+                      runSpacing: 4,
+                      children: item["routes"].map<Widget>((route) {
+                        print(item);
+                        return Container(
+                          height: 28,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 4.5,
+                            horizontal: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.blue, // Fixed color for routes
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Text(
+                            route['name'],
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              )
+            else
+              const Row(
+                children: [
+                  Icon(
+                    Icons.info_outline,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    'No routes available',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+        ],
+      ),
+    ),
+  );
+  // child: Container(
+  //   width: 150, // Adjust size based on your design
+  //   decoration: BoxDecoration(
+  //     borderRadius: BorderRadius.circular(25),
+  //     color: Colors.black,
+  //   ),
+  //   child: ElevatedButton(
+  //     style: ElevatedButton.styleFrom(
+  //       backgroundColor: Colors.transparent,
+  //       padding: const EdgeInsets.all(10),
+  //       shape: RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.circular(15),
+  //       ),
+  //     ),
+  //     onPressed: () {
+
+  //       print('$type card pressed: ${item['name']}');
+  //     },
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: [
+  //         Text(
+  //           item['name'] ?? 'Unknown $type',
+  //           style: const TextStyle(color: Colors.white),
+  //         ),
+  //         if (type == 'POI')
+  //           Text(
+  //             item['address'] ?? 'No address available',
+  //             style: const TextStyle(color: Colors.white),
+  //           ),
+  //       ],
+  //     ),
+  //   ),
+  // ),
 }
